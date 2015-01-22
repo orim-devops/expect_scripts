5c8d72728ea886bc8d38fd886e53e9fa14939b83
Rene Rodriguez1 <renerodriguez1@Renes-MacBook-Pro.local>
2015-01-22 11:04:28 -0500

    Adding a composer expect script

---
+++ composer_expect.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/expect -f
+
+set timeout 2 
+
+spawn composer install
+
+expect {
+    timeout { exit 1; }
+    "Username: *"
+}
+
+send "$gitUsername\r"
+
+expect {
+    timeout { exit 1; }
+    "Password: *"
+}
+
+send "$gitPassword\r"
